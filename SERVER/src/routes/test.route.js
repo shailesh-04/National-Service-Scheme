570938e32e3d70d7d9044cc9da12b70d97b73e22
shailesh-04 <shailesakoli@gmail.com>
2025-03-16 09:31:53 +0530

    test send otp

--- SERVER/src/routes/test.route.js
+++ SERVER/src/routes/test.route.js
@@ -3,7 +3,7 @@ const router = Router();
 import { catchErr } from "#color";
 import genarateOTP from "#services/genarateOTP.js";
 try {
-    post("/", async (req, res) => {
+    router.post("/", async (req, res) => {
         const { email } = req.body;
         if (!email) {
             return res.status(400).json({ error: "Email is required" });
